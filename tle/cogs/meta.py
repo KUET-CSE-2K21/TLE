fee21be623b3e4d17cbc6d9c50d5054450e2d6d2
Đào Lê Bảo Minh <106399179+djnner@users.noreply.github.com>
2022-05-30 10:09:28 +0700

    make it better

--- tle/cogs/meta.py
+++ tle/cogs/meta.py
@@ -3,6 +3,17 @@ import subprocess
 import sys
 import time
 import textwrap
+import logging
+import json
+import discord
+
+from tle.util import codeforces_common as cf_common
+from tle.util import codeforces_api as cf
+from tle import constants
+from tle.util import discord_common
+from tle.cogs.handles import HandleCogError,_CLIST_RESOURCE_SHORT_FORMS,_SUPPORTED_CLIST_RESOURCES
+from tle.cogs.handles import CODECHEF_RATED_RANKS
+from tle.util.codeforces_api import RATED_RANKS as CODEFORCES_RATED_RANKS
 
 from discord.ext import commands
 
@@ -43,11 +54,52 @@ def git_history():
     except OSError:
         return "Fetching git info failed"
 
+async def _create_roles(ctx, ranks):
+    for rank in ranks[::-1]:
+        guild = ctx.guild
+        await guild.create_role(name=rank.title, colour=discord.Colour(rank.color_embed))
 
 class Meta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.start_time = time.time()
+        self.converter = commands.MemberConverter()
+
+    @commands.Cog.listener()
+    @discord_common.once
+    async def on_ready(self):
+        pass
+
+    @commands.command(brief='(unofficial) Calculate a math expression', usage='[expression]')
+    @commands.is_owner()
+    async def calc(self, ctx, exp: str = '0'):
+        """Calculate a math expression (in python format).
+        Note that ALL CONSTANTS and the ANSWER must be 2000 or fewer in LENGTH.
+        e.g ;calc 10**9 + 7"""
+        await ctx.send(str(eval(exp)))
+
+    @commands.group(brief='Command to create roles for codeforces/codechef', invoke_without_command=True)
+    @commands.check_any(commands.has_any_role('Admin', constants.TLE_MODERATOR), commands.is_owner())
+    async def createroles(self, ctx):
+        await ctx.send_help(ctx.command)
+    
+    @createroles.command(brief='Create roles for codeforces ranks')
+    async def codeforces(self, ctx):
+        wait_msg = await ctx.channel.send("Creating Roles...")
+        await _create_roles(ctx, CODEFORCES_RATED_RANKS)
+        await wait_msg.delete()
+        await ctx.send(embed=discord_common.embed_success('Roles created successfully.'))
+
+    @createroles.command(brief='Create roles for codechef stars')
+    async def codechef(self, ctx):
+        wait_msg = await ctx.channel.send("Creating Roles...")
+        await _create_roles(ctx, CODECHEF_RATED_RANKS)
+        await wait_msg.delete()
+        await ctx.send(embed=discord_common.embed_success('Roles created successfully.'))
+
+    @discord_common.send_error_if(HandleCogError, cf_common.HandleIsVjudgeError)
+    async def cog_command_error(self, ctx, error):
+        pass
 
     @commands.group(brief='Bot control', invoke_without_command=True)
     async def meta(self, ctx):
