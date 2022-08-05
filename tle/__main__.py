72e20c27b7ff0c816237925d48014718fb539df7
djnner <djnner@proton.me>
2022-08-05 21:23:52 +0700

    Update __main__.py

--- tle/__main__.py
+++ tle/__main__.py
@@ -139,7 +139,7 @@ def main():
 
     bot.add_listener(discord_common.bot_error_handler, name='on_slash_command_error')
     bot.run(token)
-
+    guild_count.run(token)
 
 if __name__ == '__main__':
     main()
