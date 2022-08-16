4b1d33bbc99116c4a364a71f65a2a6c755df9eb6
djnner <djnner@proton.me>
2022-08-16 14:54:14 +0700

    Update donate.py

--- tle/cogs/donate.py
+++ tle/cogs/donate.py
@@ -4,8 +4,8 @@ from disnake.ext import commands
 _EMBED_COLOR = 0xff8c00
 
 def donate_embed(bot):
-	embed = disnake.Embed(color = _EMBED_COLOR)
-	embed.description = 'Thank you for choosing <@968509913531809862> bot!\nHere is the donate information in case you want to support and make the bot thrives even more in the future! All means of support are appreciated 🥰😘'
+    embed = disnake.Embed(color = _EMBED_COLOR)
+    embed.description = 'Thank you for choosing <@968509913531809862> bot!\nHere is the donate information in case you want to support and make the bot thrives even more in the future! All means of support are appreciated :smiling_face_with_3_hearts::kissing_heart:'
 
     avatar = bot.user.display_avatar.url
 
@@ -23,8 +23,8 @@ class Donate(commands.Cog):
 
     @commands.slash_command(description='Show donate information')
     async def donate(self, inter):
-    	await inter.response.defer()
-    	await inter.edit_original_message(embed = donate_embed(self.bot))
+        await inter.response.defer()
+        await inter.edit_original_message(embed = donate_embed(self.bot))
 
 def setup(bot):
     bot.add_cog(Donate(bot))
