5033dfdaa833777872dc4075aa51a26d5e1e8220
djnner <djnner@proton.me>
2022-09-09 23:13:25 +0700

    Update cache_control.py

--- tle/cogs/cache_control.py
+++ tle/cogs/cache_control.py
@@ -33,37 +33,39 @@ class CacheControl(commands.Cog):
 
     @cache.sub_command(description='Reload contests cache')
     @commands.is_owner()
-    @timed_command
     async def contests(self, inter):
+        await inter.response.defer()
         await cf_common.cache2.contest_cache.reload_now()
+        await inter.edit_original_message('OK');
 
     @cache.sub_command(description='Reload problems cache')
     @commands.is_owner()
-    @timed_command
     async def problems(self, inter):
+        await inter.response.defer()
         await cf_common.cache2.problem_cache.reload_now()
+        await inter.edit_original_message('OK');
 
     @cache.sub_command(description='Reload rating changes cache')
     @commands.is_owner()
-    @timed_command
     async def ratingchanges(self, inter, contest_id: int):
         """
         Defaults to 'missing'. Mode 'all' clears existing cached changes.
         Mode 'contest_id' clears existing changes with the given contest id.
         """
+        await inter.response.defer()
         count = await cf_common.cache2.rating_changes_cache.fetch_contest(contest_id)
-        await inter.send(f'Done, fetched {count} changes and recached handle ratings')
+        await inter.edit_original_message(f'Done, fetched {count} changes and recached handle ratings')
 
     @cache.sub_command(description='Reload problemsets cache')
     @commands.is_owner()
-    @timed_command
     async def problemsets(self, inter, contest_id: int = None):
         """
         Mode 'all' clears all existing cached problems. Mode 'contest_id'
         clears existing problems with the given contest id.
         """
+        await inter.response.defer()
+
         if contest_id == None:
-            inter.channel.send('This will take a while...')
             count = await cf_common.cache2.problemset_cache.update_for_all()
         else:
             try:
@@ -71,7 +73,7 @@ class CacheControl(commands.Cog):
             except ValueError:
                 return await inter.send('Invalid contest ID')
             count = await cf_common.cache2.problemset_cache.update_for_contest(contest_id)
-        await inter.send(f'Done, fetched {count} problems')
+        await inter.edit_original_message(f'Done, fetched {count} problems')
 
 def setup(bot):
     bot.add_cog(CacheControl(bot))
