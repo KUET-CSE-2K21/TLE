6924ae721d0ebc51e9d0502e00d372de94afe78b
tasmirz <tasmirz>
2022-12-16 11:37:13 +0600

    poetry reinclude

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ cd "$(dirname "$0")"
 [ -e environment ] && . ./environment
 
 while true; do
-    pip install seaborn lxml pycairo PyGObject aiocache firebase-admin recordtype requests pytz beautifulsoup4 topggpy disnake Flask
-    FONTCONFIG_FILE=$PWD/extra/fonts.conf python -m tle
+    #git pull
+    poetry install
+    FONTCONFIG_FILE=$PWD/extra/fonts.conf poetry run python -m tle
 
     (( $? != 42 )) && break
 
